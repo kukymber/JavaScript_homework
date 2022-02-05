d211cc4898b0a16ef3434584b0af0916889a8279
admin <ostap_to@mail.ru>
2022-02-05 13:21:24 +0800

    d

---
+++ Lesson_1/task_1.js
@@ -0,0 +1,4 @@
+let Tf = 0;
+Tc = +prompt('input number');
+Tf = (9 / 5) * Tc + 32;
+console.log(alert(Tf));
---
+++ Lesson_1/task_2.js
@@ -0,0 +1,5 @@
+var admin, name;
+name_first = "vasia";
+admin = name_first;
+console.log(alert(name_first));
+// I cannot input name_first on english language .
