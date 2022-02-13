87ff3327a6c56166add23df2f0dbc27667fa8dc9
admin <ostap_to@mail.ru>
2022-02-13 17:04:09 +0800

    Did homework 4

--- Lesson_4/lesson_4_task_2.js
+++ Lesson_4/lesson_4_task_2.js
@@ -1,64 +1,23 @@
-// var basket = [[1, "булка", 15, 2],[2, "колач", 45, 1],[3, "хлеб", 25, 3],[4, "печенье", 65, 1],[5, "сырник", 15, 5]];
 class PriceBasket{
-    constructor(index, items, costs, amount, x){
-        this.index = index;  // сделать автоинкрементом
+    constructor(index, items, costs, amount, basket){
+        this.index = index;
         this.items = items;
         this.costs = costs;
         this.amount = amount;
-        this.x = x;
+        this.basket = basket;
     }
-    askMore(x){
-        if (x === '-1'){
-            CountPrice.countBasketPrice;// вызвать функцию подсчета 
-        } else{
-            GetBasket();// вызвать функцию запроса товаров
-        }
-        }
-}
-
-class CountPrice extends PriceBasket{
-    constructor(index, items, costs, amount){
-        super(index, items, costs, amount)
+    getBasket(){
+        // наполняет массив данными 
+         this.basket = {'индекс': this.index,
+                     'название товара': this.item,
+                     ' стоимость товара': this.costs,
+                     'количество товара':this.amount};
     }
     countBasketPrice(){
-    var countAll = 0;
-    for (let item of basket){
-        alert(item[1]);  // выводит название товара
-        alert(item[2] * item[3]);  // выводит сумму одного товара * кол-во
-        countAll += item[2] * item[3];
-        alert(countAll);  // считает всю корзину 
-        }
-    }
-}
+        alert(this.basket);
+}}
 
-class GetBasket extends PriceBasket{
-    constructor(index, items, costs, amount){
-        super(index, items, costs, amount)
-    }
-    getBasket(index, items, costs, amount){
-       // наполняет массив данными 
-        basket = {};  // 
-        databasket = {};
-        databasket.push(index), databasket.push(items), databasket.push(costs), databasket.push(amount);
-        basket.push(databasket);
-    }
-}
-
-// class AskMore extends PriceBasket{
-//     // нужно ли продолжать цикл наполнение или вывести результат
-//     constructor()
-//     askMore(x){
-//     if (x === '-1'){
-//         CountPrice;// вызвать функцию подсчета 
-//     } else{
-//         GetBasket;// вызвать функцию запроса товаров
-//     }
-//     }
-// }
-
-const chris = new PriceBasket(+prompt("Введите число"), prompt("Введите название товара"),
-    +prompt("Введите цену товара"), +prompt("Введите кол-во товара"), prompt())
- const getbask = new GetBasket();
-  alert(chris.askMore());
- alert(chris);
- alert(getbask.getBasket());
+let drug = new PriceBasket();
+drug.getBasket(+prompt("Введите число"), prompt("Введите название товара"),
++prompt("Введите цену товара"), +prompt("Введите кол-во товара"));
+alert(drug.countBasketPrice());
